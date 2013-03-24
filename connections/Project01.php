6cb3abea3cb67cd055399f11bdca6ee1739632cd
james.evin.coll@gmail.com <james.evin.coll@gmail.com>
2013-03-24 21:26:31 +0000

    Removed login information

--- connections/Project01.php
+++ connections/Project01.php
@@ -2,10 +2,10 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_Project01 = "msc63.cmsmotion.com";
-$database_Project01 = "msc63cms_kraftwerk";
-$username_Project01 = "msc63cms";
-$password_Project01 = "95288198";
+$hostname_Project01 = "*";
+$database_Project01 = "*";
+$username_Project01 = "*";
+$password_Project01 = "*";
 $Project01 = mysql_pconnect($hostname_Project01, $username_Project01, $password_Project01) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
 
